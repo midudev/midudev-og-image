55bad17c3c7a5b575ce4c95f935cdec47795ee0d
midudev <miduga@gmail.com>
2020-04-14 00:56:09 +0200

    Simplify request by using query

--- api/_parseRequest.js
+++ api/_parseRequest.js
@@ -2,30 +2,16 @@ import { parse } from 'url'
 
 const getTagImage = tag => `https://midu.dev/images/tags/${tag}.png`
 const getTagImages = tags => tags.map(getTagImage)
-export function parseRequest ({ url }) {
-  console.log('HTTP ' + url)
-  const { pathname = '/', query = {} } = parse(url || '', true)
-  const { tags } = query
+export function parseRequest ({ url = '' }) {
+  const { query = {} } = parse(url, true)
+  const { tags, text = '', type = 'jpeg' } = query
 
-  let extension = ''
-  let text = ''
-
-  if (typeof pathname === 'string') {
-    const arr = pathname.slice(1).split('.')
-    if (arr.length === 0) {
-      text = ''
-    } else if (arr.length === 1) {
-      text = arr[0]
-    } else {
-      extension = arr.pop()
-      text = arr.join('.')
-    }
-  } else {
-    throw new Error('Expected a string as pathname')
+  if (Array.isArray(text)) {
+    throw new Error('Expected a single text')
   }
 
   return {
-    fileType: extension === 'jpeg' ? extension : 'png',
+    fileType: type === 'jpeg' ? type : 'png',
     text: decodeURIComponent(text),
     images: getTagImages(getArray(tags))
   }
